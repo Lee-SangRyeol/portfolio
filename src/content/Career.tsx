5af9ea654a570f9bd03b80944b54e69bc391e14a
Lee-SangRyeol <tkdfuf7600@naver.com>
2024-08-23 17:49:25 +0900

    [FEAT] : career Done

---
+++ src/content/Career.tsx
@@ -0,0 +1,128 @@
+import styled from "styled-components";
+import { colors } from "../constants/index";
+
+const Career = () => {
+  return (
+    <AllContainer>
+      <TopContainer>
+        <DateText>2022.05 - 2023.07</DateText>
+        <CompanyBox>
+          <CompanyName>{"(주)"}에이락</CompanyName>
+          <Position>개발팀 - 선임</Position>
+        </CompanyBox>
+        <WorkList>
+          회사소개
+          <GrayText>
+            에이락 월렛 : 블록체인 기반의 핀테크 회사로 핀테크협회 부회장사를
+            역임.
+            <br />
+            지갑 서비스 “에이락월렛”으로 제휴사를 통해 지급되는 리워드를
+            가상자산으로 지급 및 관리.
+          </GrayText>
+          <GrayText>
+            마이메타갤러리 : 그림에 NFT를 발행하여 작품에 디지털 자산을 더하는
+            서비스. 웹 사생대회를 통해 작품 등록 및 전시를 진행하였으며,
+            오프라인으로 국내 작가와의 콜라보를 진행.
+          </GrayText>
+        </WorkList>
+        <WorkList>
+          담당업무
+          <GrayText>
+            백오피스 개발(1인)
+            <TextUl>
+              <GrayText>
+                에이락 월렛과 마이메타갤러리 두 서비스의 백오피스 개발 및
+                유지보수
+              </GrayText>
+              <GrayText>
+                대시보드, 리워드 지급 관리 기능, 마켓의 상품관리 기능등 권한에
+                따른 백오피스 기능 개발 및 유지보수
+              </GrayText>
+            </TextUl>
+          </GrayText>
+          <GrayText>
+            백엔드 개발 및 유지보수(4인)
+            <TextUl>
+              <GrayText>내부용 GraphQl쿼리 개발 및 유지보수</GrayText>
+              <GrayText>외부용 REST API 개발 및 유지보수</GrayText>
+              <GrayText>DB 구조 변경 및 마이그레이션 진행</GrayText>
+            </TextUl>
+          </GrayText>
+          <GrayText>
+            제휴사 및 CS 대행업체 관리 및 커뮤니케이션
+            <TextUl>
+              <GrayText>
+                제휴사와 협업시 각종 요구사항 정리 및 문제점 조율
+              </GrayText>
+              <GrayText>CS대행업체의 관리 및 월렛 서비스 이슈 관리</GrayText>
+            </TextUl>
+          </GrayText>
+        </WorkList>
+      </TopContainer>
+    </AllContainer>
+  );
+};
+
+export default Career;
+
+const AllContainer = styled.div`
+  width: 500px;
+  height: 550px;
+  background-color: ${colors.grayscale.$02};
+  padding: 20px;
+  border-radius: 5px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+`;
+
+const TopContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+`;
+
+const DateText = styled.div`
+  font-size: 14px;
+  color: ${colors.grayscale.$07};
+`;
+
+const CompanyBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 7px;
+`;
+
+const CompanyName = styled.div`
+  font-size: 24px;
+  color: ${colors.secondary.white};
+`;
+
+const Position = styled.div`
+  font-size: 14px;
+  color: ${colors.secondary.white};
+`;
+
+const WorkList = styled.ul`
+  font-size: 14px;
+  color: ${colors.grayscale.$09};
+  padding-left: 20px;
+  margin: 0;
+  display: flex;
+  flex-direction: column;
+  gap: 4px;
+`;
+
+const GrayText = styled.li`
+  font-size: 14px;
+  color: ${colors.grayscale.$07};
+  list-style-type: disc;
+  line-height: 20px;
+`;
+
+const TextUl = styled.ul`
+  padding-left: 20px;
+  list-style-type: circle;
+`;
+
+const BottomContainer = styled.div``;
---
+++ src/sections/Section3.tsx
@@ -0,0 +1,178 @@
+import React, { useState, useEffect } from "react";
+import styled, { keyframes } from "styled-components";
+import { colors } from "../constants/index";
+import Career from "../content/career";
+
+const Section3 = () => {
+  const [activeSection, setActiveSection] = useState<string>("");
+
+  const scrollToSection = (sectionId: string) => {
+    const section = document.getElementById(sectionId);
+    if (section) {
+      section.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
+  useEffect(() => {
+    const sections = ["career", "project", "education"];
+    const options = {
+      root: null,
+      threshold: 0.5,
+    };
+
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          setActiveSection(entry.target.id);
+        }
+      });
+    }, options);
+
+    sections.forEach((section) => {
+      const element = document.getElementById(section);
+      if (element) {
+        observer.observe(element);
+      }
+    });
+
+    return () => {
+      sections.forEach((section) => {
+        const element = document.getElementById(section);
+        if (element) {
+          observer.unobserve(element);
+        }
+      });
+    };
+  }, []);
+
+  return (
+    <Container>
+      <Sidebar>
+        <SidebarItem
+          onClick={() => scrollToSection("career")}
+          $active={activeSection === "career"}
+        >
+          Career
+        </SidebarItem>
+        <SidebarItem
+          onClick={() => scrollToSection("project")}
+          $active={activeSection === "project"}
+        >
+          Project
+        </SidebarItem>
+        <SidebarItem
+          onClick={() => scrollToSection("education")}
+          $active={activeSection === "education"}
+        >
+          Education
+        </SidebarItem>
+      </Sidebar>
+      <Content>
+        <Section
+          id="career"
+          className={activeSection === "career" ? "fade-in" : "fade-out"}
+        >
+          <Career></Career>
+        </Section>
+        <Section
+          id="project"
+          className={activeSection === "project" ? "fade-in" : "fade-out"}
+        >
+          <GrayContainer>project</GrayContainer>
+        </Section>
+        <Section
+          id="education"
+          className={activeSection === "education" ? "fade-in" : "fade-out"}
+        >
+          <GrayContainer>education</GrayContainer>
+          <GrayContainer>education</GrayContainer>
+        </Section>
+      </Content>
+    </Container>
+  );
+};
+
+export default Section3;
+
+const fadeIn = keyframes`
+  from {
+    opacity: 0;
+    transform: translateY(-80px);
+  }
+  to {
+    opacity: 1;
+    transform: translateY(0);
+  }
+`;
+
+const fadeOut = keyframes`
+  from {
+    opacity: 1;
+  }
+  to {
+    opacity: 0;
+  }
+`;
+
+const Container = styled.div`
+  width: 100%;
+  height: 322vh;
+  display: flex;
+  background-color: ${colors.grayscale.$01};
+`;
+
+const Sidebar = styled.div`
+  width: 200px;
+  height: 100vh;
+  position: sticky;
+  top: 35px;
+  display: flex;
+  flex-direction: column;
+  padding-left: 40px;
+`;
+
+const SidebarItem = styled.div<{ $active: boolean }>`
+  color: ${(props) =>
+    props.$active ? colors.secondary.white : colors.grayscale.$02};
+  font-size: 60px;
+  font-weight: 700;
+  margin-bottom: 30px;
+  cursor: pointer;
+
+  &:hover {
+    color: ${(props) =>
+      props.$active ? colors.secondary.white : colors.grayscale.$03};
+  }
+`;
+
+const Content = styled.div`
+  flex-grow: 1;
+  padding-left: 80px;
+`;
+
+const Section = styled.div`
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 20px;
+  transition: opacity 1s ease-in-out, transform 1s ease-in-out;
+
+  &.fade-in {
+    animation: ${fadeIn} 0.5s ease-in forwards;
+  }
+
+  &.fade-out {
+    animation: ${fadeOut} 0.5s ease-out forwards;
+  }
+`;
+
+const GrayContainer = styled.div`
+  background-color: ${colors.grayscale.$02};
+  padding: 20px;
+  border-radius: 5px;
+  width: 500px;
+  height: 480px;
+  font-size: 48px;
+  color: ${colors.secondary.white};
+`;
